d9fdc2b85d50ed13b0d0d06b8cd796ae0b96e523
Muhammad Faizan <faizansohail077@gmail.com>
2020-12-03 00:17:47 -0500

    redux-toolkit

---
+++ store/state.ts
@@ -0,0 +1,26 @@
+import { ProductItem } from "../global"
+
+const INITIAL_STATE: ProductItem[] = [
+  {
+    id: "123",
+    title: "Blue t-shirt",
+    description: "no fancy t-shirts avaliable",
+    imageUrl: "/blue-tshirt.png",
+    price: 399
+  },
+  {
+    id: "456",
+    title: "yellow t-shirt",
+    description: "no fancy t-shirts avaliable",
+    imageUrl: "/yellow-tshirt.png",
+    price: 399
+  },
+  {
+    id: "789",
+    title: "red t-shirt",
+    description: "no fancy t-shirts avaliable",
+    imageUrl: "/red-tshirt.png",
+    price: 399
+  }
+]
+export { INITIAL_STATE }
